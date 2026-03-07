309f50998f45ddc82a9c522cd2d69ce4688b758c
ZH <44901162+kiweeone@users.noreply.github.com>
2026-03-07 05:57:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './Landing.jsx'
+import F1Predictor from './F1Predictor.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/f1predictor" element={<F1Predictor />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
